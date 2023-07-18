b111ef69965793150ddcab7da05ec2d06a975994
Kleber Barreto de Macedo <kleber.barreto@msn.com>
2023-07-18 18:51:18 -0300

    Adicionada rota

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React from 'react';
 import './global.css';
-import Login from './pages/login';
+import Routes from './routes';
 
 export default function App() {
   return (
-  <Login/>
+  <Routes/>
   );
 }
