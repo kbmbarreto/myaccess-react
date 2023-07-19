e10c050a3aa08660f1a500ceb3e296a2f5578e98
Kleber Barreto de Macedo <kleber.barreto@msn.com>
2023-07-19 17:28:23 -0300

    Adicionada visualização de itens do banco de dados

--- src/pages/passwords/index.js
+++ src/pages/passwords/index.js
@@ -1,15 +1,37 @@
+import { useState, useEffect } from "react";
 import React from "react";
-import './style.css';
 import logoImage from '../../assets/java.png';
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { FiPower, FiEdit, FiTrash2 } from "react-icons/fi";
 
+import api from "../../services/api";
+
+import './style.css';
+
 export default function Passwords() {
+
+    const [passwords, setPasswords] = useState([]);
+
+    const email = localStorage.getItem('email');
+    const token = localStorage.getItem('token');
+
+    const history = useHistory();
+
+    useEffect(() => {
+        api.get('password/1', {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            }
+        }).then(response => {
+            setPasswords(response.data);
+        })
+    })
+
     return (
         <div className="password-container">
             <header>
                 <img src={logoImage} alt="Java" />
-                <span>Bem-vindo, <strong>Kleber</strong>!</span>
+                <span>Bem-vindo, <strong>{email.toLowerCase()}</strong> !</span>
                 <Link className="button" to="passwords/new">Cadastrar nova senha</Link>
                 <button type="button">
                     <FiPower size={18} color="#E02041" />
@@ -18,89 +40,28 @@ export default function Passwords() {
 
             <h1>Senhas cadastradas</h1>
             <ul>
-                <li>
-                    <strong>Descrição:</strong>
-                    <p>Santander</p>
-                    <strong>Url:</strong>
-                    <p>https://santander.com.br</p>
-                    <strong>Usuário:</strong>
-                    <p>1234567-8</p>
-                    <strong>Senha:</strong>
-                    <p>000000</p>
-                    <strong>Anotações:</strong>
-                    <p>Senha eletrônica: 777777</p>
-
-                    <button type="button">
+                {passwords.map(password => (
+                    <li>
+                        <strong>Descrição:</strong>
+                        <p>{password.description}</p>
+                        <strong>Url:</strong>
+                        <p>{password.url}</p>
+                        <strong>Usuário:</strong>
+                        <p>{password.username}</p>
+                        <strong>Senha:</strong>
+                        <p>{password.password}</p>
+                        <strong>Anotações:</strong>
+                        <p>{password.notes}</p>
+                
+                        <button type="button">
                         <FiEdit size={20} color="#a8a8b3" />
-                    </button>
-
-                    <button type="button">
-                        <FiTrash2 size={20} color="#a8a8b3" />
-                    </button>
-                </li>
-
-                <li>
-                    <strong>Descrição:</strong>
-                    <p>Santander</p>
-                    <strong>Url:</strong>
-                    <p>https://santander.com.br</p>
-                    <strong>Usuário:</strong>
-                    <p>1234567-8</p>
-                    <strong>Senha:</strong>
-                    <p>000000</p>
-                    <strong>Anotações:</strong>
-                    <p>Senha eletrônica: 777777</p>
-
-                    <button type="button">
-                        <FiEdit size={20} color="#a8a8b3" />
-                    </button>
-
-                    <button type="button">
-                        <FiTrash2 size={20} color="#a8a8b3" />
-                    </button>
-                </li>
-
-                <li>
-                    <strong>Descrição:</strong>
-                    <p>Santander</p>
-                    <strong>Url:</strong>
-                    <p>https://santander.com.br</p>
-                    <strong>Usuário:</strong>
-                    <p>1234567-8</p>
-                    <strong>Senha:</strong>
-                    <p>000000</p>
-                    <strong>Anotações:</strong>
-                    <p>Senha eletrônica: 777777</p>
-
-                    <button type="button">
-                        <FiEdit size={20} color="#a8a8b3" />
-                    </button>
-
-                    <button type="button">
-                        <FiTrash2 size={20} color="#a8a8b3" />
-                    </button>
-                </li>
-
-                <li>
-                    <strong>Descrição:</strong>
-                    <p>Santander</p>
-                    <strong>Url:</strong>
-                    <p>https://santander.com.br</p>
-                    <strong>Usuário:</strong>
-                    <p>1234567-8</p>
-                    <strong>Senha:</strong>
-                    <p>000000</p>
-                    <strong>Anotações:</strong>
-                    <p>Senha eletrônica: 777777</p>
-
-                    <button type="button">
-                        <FiEdit size={20} color="#a8a8b3" />
-                    </button>
-
-                    <button type="button">
-                        <FiTrash2 size={20} color="#a8a8b3" />
-                    </button>
-                </li>
+                        </button>
+                
+                        <button type="button">
+                            <FiTrash2 size={20} color="#a8a8b3" />
+                        </button>
+                    </li>
+                ))}
             </ul>
         </div>
     )
